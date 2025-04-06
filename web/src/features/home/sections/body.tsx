3af7dcee68703d0a4ff3999f5dec59629d489327
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2025-04-06 19:12:50 +0100

    added feature: add new section to home page

--- web/src/features/home/sections/body.tsx
+++ web/src/features/home/sections/body.tsx
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 
 import { mockData } from "../sections/mockdata.json";
 
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, Button } from "@mui/material";
 import image1 from "../../../assets/download.jpeg";
 
 export default function HeroBanner() {
@@ -20,10 +20,23 @@ export default function HeroBanner() {
     <Box sx={{ flexGrow: 1, padding: 2 }}>
       <Grid container spacing={3}>
         {data.map((item, index) => (
-            <Box sx={{ border: "1px solid #ccc", padding: 2, borderRadius: 2 }}>
+            <Box sx={{ border: "1px solid #ccc", padding: 2, borderRadius: 2, display: "flex", flexDirection: "column", alignItems: "center" }} key={index}>
               <Typography variant="h6">{item.title}</Typography>
               <Typography variant="body2">{item.description}</Typography>
               <img src={image1} alt={item.title} style={{ width: "200px", borderRadius: "8px" }} />
+              
+             <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop: 2, gap: 1 }}>
+            <Button variant="contained" color="primary" key={index} style={{ marginTop: "10px" }}>  
+              delete
+            </Button>
+            <Button variant="contained" color="primary" key={index} style={{ marginTop: "10px" }}>  
+              edit
+            </Button>
+            </Box>
+            <Box sx={{ flexGrow: 1 }} />
+            <Button variant="contained" color="primary" key={index} style={{ marginTop: "10px" }}>  
+              view book
+            </Button>
             </Box>
         ))}
       </Grid>
