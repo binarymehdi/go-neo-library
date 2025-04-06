0a26206c0339f3bfeb658142b7633f09f079f04a
El Mehdi Laidoudi <elmehdilaidoudi@gmail.com>
2025-04-06 18:11:05 +0100

    web foundation

---
+++ web/src/main.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/hi",
+    element: <div>Hello world!!!</div>,
+  },
+]); 
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+      </ThemeProvider>
+  </React.StrictMode>,
+);
